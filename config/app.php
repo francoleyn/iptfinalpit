6bbef16b5e355358fe3ba863609a498d6e3405a0
Faisal R. Inidal <inidal.faisal8@gmail.com>
2026-05-29 01:00:09 +0800

    update

--- config/app.php
+++ config/app.php
@@ -9,7 +9,7 @@ return [
     |
     | This value is the name of your application, which will be used when the
     | framework needs to place the application's name in a notification or
-    | other UI elements where an application name needs to be displayed.
+    | other UI elements where an application name needs to be displayed
     |
     */
 
@@ -22,7 +22,7 @@ return [
     |
     | This value determines the "environment" your application is currently
     | running in. This may determine how you prefer to configure various
-    | services the application utilizes. Set this in your ".env" file.
+    | services the application utilizes. Set this in your ".env" file
     |
     */
 
